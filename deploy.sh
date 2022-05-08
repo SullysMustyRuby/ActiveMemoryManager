58785073f16ed6affc7ea6ba46e5e91b803602dc
erin boeger <erin.boeger@erlang-solutions.com>
2022-05-08 21:28:49 +0900

    updated for sully home dir

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Path variables
-mnesia_manager_path="/home/ec2-user/mnesia-manager/"
-mnesia_manager_backup_path="/home/ec2-user/release-backups/mnesia-manager/"
-mnesia_manager_build_path="/home/ec2-user/mnesia-manager/_build/prod/rel/mnesia_manager/"
-mnesia_manager_env="/home/ec2-user/mnesia-manager/.env.prod"
-mnesia_manager_service_path="/home/ec2-user/mnesia-manager/_build/prod/rel/mnesia_manager/bin/mnesia_manager"
+mnesia_manager_path="/home/sullymustycode/mnesia-manager/"
+mnesia_manager_backup_path="/home/sullymustycode/release-backups/mnesia-manager/"
+mnesia_manager_build_path="/home/sullymustycode/mnesia-manager/_build/prod/rel/mnesia_manager/"
+mnesia_manager_env="/home/sullymustycode/mnesia-manager/.env.prod"
+mnesia_manager_service_path="/home/sullymustycode/mnesia-manager/_build/prod/rel/mnesia_manager/bin/mnesia_manager"
 
 # Functions
 backup_release() {
